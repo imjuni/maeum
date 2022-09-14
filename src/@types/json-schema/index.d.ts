84e0e11f7ebbd4f150afd9919b465374c74a7f6e
ByungJoon Lee <imjuni@gmail.com>
2022-09-15 00:25:32 +0900

    Fix build error
    * add @types directory on tsconfig.json file
    * add types(jest) on tsconfig.json file

--- src/@types/json-schema/index.d.ts
+++ src/@types/json-schema/index.d.ts
@@ -3,7 +3,7 @@ import 'json-schema';
 declare module 'json-schema' {
   export interface JSONSchema7 {
     dateformatex?: string;
-    example: string;
+    example?: string;
     collectionFormat?: 'csv' | 'ssv' | 'tsv' | 'pipes' | 'multi';
   }
 }
